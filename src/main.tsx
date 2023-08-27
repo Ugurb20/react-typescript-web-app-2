a6c4e19e52c7e6c71611a65960ff03ef6cadb85e
Ugurb20 <ugurbukcuoglu@gmail.com>
2023-08-27 12:53:07 +0300

    firebase excluded

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,15 @@ import './index.css';
 import store from './store/store';
 import { Provider as InversionProvider } from 'inversify-react';
 import { getContainer } from '@utils/inversion-container';
-import { FirebaseProvider } from '@hooks/firebase-context';
+
 import { LoadingOverlayProvider } from '@components/loading/loading-overlay/context';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<InversionProvider
-  container={getContainer()}>
-  <LoadingOverlayProvider>
-    <FirebaseProvider>
-
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <InversionProvider container={getContainer()}>
+    <LoadingOverlayProvider>
       <Provider store={store}>
         <App />
       </Provider>
-    </FirebaseProvider>
-  </LoadingOverlayProvider>
-</InversionProvider>);
+    </LoadingOverlayProvider>
+  </InversionProvider>,
+);
